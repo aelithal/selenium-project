7df818ac2dff39fac5c5af700bec54f230c946fc
Eva PinkXray <eva.pink.xray@gmail.com>
2026-03-06 10:05:45 +0300

    feat: add user add product test

--- pages/locators.py
+++ pages/locators.py
@@ -4,10 +4,15 @@ class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_invalid")
     BASKET_LINK = (By.XPATH, "//*[@id='default']/header/div[1]/div/div[2]/span/a")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 class LoginPageLocators():
     LOGIN_FORM = (By.ID, "login_form")
     REGISTRATION_FORM = (By.ID, "register_form")
+    EMAIL_INPUT = (By.ID, "id_registration-email")
+    PASSWORD_INPUT = (By.ID, "id_registration-password1")
+    PASSWORD_CONFIRM = (By.ID, "id_registration-password2")
+    REGISTER_BUTTON = (By.CSS_SELECTOR, "button[value='Register']")
 
 class ProductPageLocators():
     ADD_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form > button")
--- test_product_page.py
+++ test_product_page.py
@@ -12,7 +12,6 @@ def test_guest_can_add_product_to_basket(browser):
     page.should_be_add_button()
     page.click_button()
     page.solve_quiz_and_get_code()
-    time.sleep(30)
 
 @pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
@@ -31,3 +30,20 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page.go_to_basket_page()
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.is_basket_empty()
+
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        self.browser = browser
+        link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+        page = LoginPage(self.browser, link)
+        page.open()
+        page.register_new_user(str(time.time()) + "@fake.com", "1a3b5c7d9")
+        page.should_be_authorized_user()
+
+    @pytest.mark.need_review
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_add_button()
